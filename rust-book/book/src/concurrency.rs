565447c3220d8970f858805460291c8553634a6c
Theodor <ftri2001@gmail.com>
2022-07-11 18:12:19 +0300

    Completed concurrency chapter

---
+++ rust-book/book/src/concurrency.rs
@@ -0,0 +1,70 @@
+use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+use std::time::Duration;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+
+    // ** Using Message Passing to Transfer Data Between Threads ** //
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(400));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(400));
+        }
+    });
+
+    // let received = rx.recv().unwrap(); or let received = rx.try_recv().unwrap();
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // ** Shared-State Concurrency ** //
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
