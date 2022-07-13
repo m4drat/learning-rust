31f508f723d012a35397708bf722f5c35fd0e979
Theodor <ftri2001@gmail.com>
2022-07-13 13:50:18 +0300

    Completed exercises from exercism:
    - Health Statistics
    - Low-Power Embedded Game

---
+++ exercism-solutions/rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,20 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(i, val)| val)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        (self.0 - 0).abs() + (self.1 - 0).abs()
+    }
+}
