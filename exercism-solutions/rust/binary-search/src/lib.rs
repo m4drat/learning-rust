febb223cafb38b9bdae770f475e9de39425cb598
Theodor Arsenij <ftri2001@gmail.com>
2024-01-02 18:39:37 +0100

    'binary-search' - generic solution

--- exercism-solutions/rust/binary-search/src/lib.rs
+++ exercism-solutions/rust/binary-search/src/lib.rs
@@ -1,48 +1,45 @@
-// Boring implementation
+// Boring (but somewhat clean) implementation
 // pub fn find(array: &[i32], key: i32) -> Option<usize> {
-//     if array.is_empty() {
-//         return None;
-//     }
+//     let mut left = 0;
+//     let mut right = array.len();
 
-//     let mut left: i64 = 0;
-//     let mut right: i64 = (array.len() - 1) as i64;
-
-//     while left <= right {
+//     while left < right {
 //         let mid = (left + right) / 2;
-//         let value = array[mid as usize];
+//         let value = array[mid];
 
 //         if value == key {
-//             return Some(mid as usize);
+//             return Some(mid);
 //         }
 
 //         if value < key {
 //             left = mid + 1;
 //         } else if value > key {
-//             right = mid - 1;
+//             right = mid;
 //         }
 //     }
 
 //     None
 // }
 
-// Boring implementation
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+use std::cmp::Ordering;
+
+// Not so boring implementation
+pub fn find<KeyType, CollectionType>(array: CollectionType, key: KeyType) -> Option<usize>
+where
+    KeyType: Ord,
+    CollectionType: AsRef<[KeyType]>,
+{
+    let array = array.as_ref();
     let mut left = 0;
     let mut right = array.len();
 
     while left < right {
         let mid = (left + right) / 2;
-        let value = array[mid];
-
-        if value == key {
-            return Some(mid);
-        }
-
-        if value < key {
-            left = mid + 1;
-        } else if value > key {
-            right = mid;
-        }
+        match array[mid].cmp(&key) {
+            Ordering::Equal => return Some(mid),
+            Ordering::Less => left = mid + 1,
+            Ordering::Greater => right = mid,
+        };
     }
 
     None
