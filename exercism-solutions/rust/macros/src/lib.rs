f46c98190a1ab6d7db1a552b926171a4a7d92a3e
Theodor Arsenij <ftri2001@gmail.com>
2024-01-08 17:25:13 +0100

    `macros` - code simplifications

--- exercism-solutions/rust/macros/src/lib.rs
+++ exercism-solutions/rust/macros/src/lib.rs
@@ -3,10 +3,9 @@ macro_rules! hashmap {
     () => {
         ::std::collections::HashMap::new()
     };
-    ($key0:expr => $val0:expr $(, $keyn:expr => $valn:expr)* $(,)? ) => {
+    ($($keyn:expr => $valn:expr),+ $(,)? ) => {
         ::std::collections::HashMap::from(
             [
-                ($key0, $val0),
                 $(($keyn, $valn)),*
             ]
         )
