47b2e49f851a7406d554a37bba0b652831a5d5ce
Theodor Arsenij <ftri2001@gmail.com>
2024-01-09 17:44:33 +0100

    `simple-linked-list` - added a solution

--- exercism-solutions/rust/simple-linked-list/src/lib.rs
+++ exercism-solutions/rust/simple-linked-list/src/lib.rs
@@ -1,50 +1,118 @@
-use std::iter::FromIterator;
+use std::{default, iter::FromIterator, mem::take};
 
+pub struct Node<T> {
+    next: MaybeNode<T>,
+    value: T,
+}
+
+type MaybeNode<T> = Option<Box<Node<T>>>;
+
+#[derive(Default)]
 pub struct SimpleLinkedList<T> {
-    // Delete this field
-    // dummy is needed to avoid unused parameter error during compilation
-    dummy: ::std::marker::PhantomData<T>,
+    head: MaybeNode<T>,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        todo!()
+        Self { head: None }
     }
 
-    // You may be wondering why it's necessary to have is_empty()
-    // when it can easily be determined from len().
-    // It's good custom to have both because len() can be expensive for some types,
-    // whereas is_empty() is almost always cheap.
-    // (Also ask yourself whether len() is expensive for SimpleLinkedList)
     pub fn is_empty(&self) -> bool {
-        todo!()
+        self.head.is_none()
     }
 
     pub fn len(&self) -> usize {
-        todo!()
+        // We could've stored a field `total_nodes`, but I don't want to
+        // do it here :)
+        let mut curr = &self.head;
+        let mut length: usize = 0;
+        while curr.is_some() {
+            curr = &curr.as_ref().unwrap().next;
+            length += 1;
+        }
+
+        length
+    }
+
+    /// Returns a mutable reference to the last node with a `None` next field.
+    fn last_mut(&mut self) -> &mut MaybeNode<T> {
+        if self.head.is_none() {
+            return &mut self.head;
+        }
+
+        let mut curr = &mut self.head;
+        while curr.as_mut().unwrap().next.is_some() {
+            curr = &mut curr.as_mut().unwrap().next;
+        }
+
+        curr
+    }
+
+    fn last(&self) -> &MaybeNode<T> {
+        if self.head.is_none() {
+            return &self.head;
+        }
+
+        let mut curr = &self.head;
+        while curr.as_ref().unwrap().next.is_some() {
+            curr = &curr.as_ref().unwrap().next;
+        }
+
+        curr
     }
 
     pub fn push(&mut self, _element: T) {
-        todo!()
+        let last = self.last_mut();
+        if last.is_none() {
+            *last = Some(Box::new(Node {
+                next: None,
+                value: _element,
+            }));
+        } else {
+            let last = last.as_mut().unwrap();
+            last.next = Some(Box::new(Node {
+                next: None,
+                value: _element,
+            }));
+        }
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        todo!()
+        let last = self.last_mut();
+        if last.is_none() {
+            return None;
+        }
+
+        let last = last.take().unwrap();
+        Some(last.value)
     }
 
     pub fn peek(&self) -> Option<&T> {
-        todo!()
+        let last = self.last();
+        if last.is_none() {
+            return None;
+        }
+
+        let last = last.as_ref().unwrap();
+        Some(&last.value)
     }
 
     #[must_use]
     pub fn rev(self) -> SimpleLinkedList<T> {
-        todo!()
+        let elements: Vec<T> = self.into();
+        elements.into_iter().rev().collect()
     }
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
     fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-        todo!()
+        let mut list = Self::new();
+
+        for item in _iter {
+            list.push(item);
+        }
+
+        list
     }
 }
 
@@ -61,6 +129,12 @@ impl<T> FromIterator<T> for SimpleLinkedList<T> {
 
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
     fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
-        todo!()
+        let mut vec = Vec::new();
+        while let Some(item) = _linked_list.pop() {
+            vec.push(item);
+        }
+
+        vec.reverse();
+        vec
     }
 }
